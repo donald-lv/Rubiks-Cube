83d9611fa4e5e3e981e9c2172788fce0476751b3
donald <d2lv@uwaterloo.ca>
2023-01-14 23:06:34 -0500

    updated database

--- server/database.sql
+++ server/database.sql
@@ -2,8 +2,16 @@ CREATE DATABASE rubikscube;
 
 CREATE TABLE IF NOT EXISTS time_submissions(
     id SERIAL PRIMARY KEY,
-    submission_time TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+
+    user_name TEXT NOT NULL,
+    
     start_time TIMESTAMP(4) NOT NULL,
+    
     end_time TIMESTAMP(4) NOT NULL,
+    
+    submission_time TIMESTAMP(0) NOT NULL DEFAULT NOW(),
+
+    move_count INTEGER NOT NULL CHECK (move_count > 0),
+
     CHECK (end_time > start_time)
 );
