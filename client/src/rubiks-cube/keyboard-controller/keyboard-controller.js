64d5c0c86e1c3635701c7f851c3f3712dad699b4
donald <d2lv@uwaterloo.ca>
2023-01-29 06:50:38 -0500

    changed: provide kb controller with binds from widget

--- client/src/rubiks-cube/keyboard-controller/keyboard-controller.js
+++ client/src/rubiks-cube/keyboard-controller/keyboard-controller.js
@@ -1,12 +1,79 @@
 import React from "react";
 import './keyboard-controller.css'
 
-function KeyboardController (props) {
+
+/* FOR KEYBOARD CODES CHECK https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values */
+
+function KeyboardControllerCard (props) {
+    function keysAreHeld (keysHeld, keys) {
+        for (const char of keys) {
+            if (!keysHeld.has(char)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
     return (
-        <div>
-            HELLO
-        </div>
+        <div className={"keyboard-tester-card" + ((keysAreHeld(props.keysHeld, props.bindingKeys)) ? " keyboard-tester-card-pulse" : "")} />
     );
+}
+
+class KeyboardController extends React.Component {
+    constructor(props) {
+        console.log("kb created");
+        super(props);
+
+        this.state = {
+            keysHeld: new Set()
+        };
+
+        document.addEventListener('keydown', (e) => {
+            let newKeysHeld = this.state.keysHeld;
+            newKeysHeld.add(e.key);
+            this.setState({ keysHeld: newKeysHeld });
+            this.checkKeys();
+        });
+
+        document.addEventListener('keyup', (e) => {
+            let newKeysHeld = this.state.keysHeld;
+            newKeysHeld.delete(e.key);
+            this.setState({ keysHeld: newKeysHeld });
+        });
+    }
+
+    keysAreHeld (keysHeld, keys) {
+        for (const char of keys) {
+            if (!keysHeld.has(char)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkKeys() {
+        for (const binding of this.props.bindings) {
+            if (this.keysAreHeld(this.state.keysHeld, binding.keys)) {
+                console.log(binding.detail);
+                const e = new CustomEvent(this.props.id + '-bind-pressed', { detail: binding.detail });
+                document.dispatchEvent(e);
+            }
+        }
+    }
+
+    render () {
+        return (
+            <div className="keyboard-tester">
+                {
+                    this.props.bindings.map(
+                        (item, index) => (<KeyboardControllerCard key={ index } keysHeld={ this.state.keysHeld } bindingKeys={ item.keys } />)
+                    )
+                }
+                
+            </div>
+        );
+    }
 }
 
 export default KeyboardController;
--- client/src/rubiks-cube/rubiks-cube-widget.js
+++ client/src/rubiks-cube/rubiks-cube-widget.js
@@ -3,8 +3,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 import './cube/rubiks-cube.js';
 import Cube from './cube/rubiks-cube.js';
 import Timer from './timer/timer.js';
+import KeyboardController from './keyboard-controller/keyboard-controller.js';
 
 import './rubiks-cube-widget.css';
+
+import bindingsImport from './controls.json';
+
+const { bindings } = bindingsImport;
 
 function CubeWidget(props) {
     const [ timerReady, setTimerReady ] = useState(false);
@@ -14,6 +19,7 @@ function CubeWidget(props) {
     const resetId = props.id + '-reset';
     const cubeId = props.id + '-cube';
     const timerId = props.id + '-timer';
+    const kbControllerId = props.id + '-kb-controller';
 
     const onFinish = useCallback((event) => {
         try {
@@ -22,8 +28,6 @@ function CubeWidget(props) {
                 start_time: new Date(event.detail.startTime).toISOString(), 
                 end_time: new Date(event.detail.endTime).toISOString()
             };
-
-            console.log(body.start_time);
 
             fetch("http://localhost:5000/leaderboard", { 
                 method: "POST", 
@@ -53,7 +57,6 @@ function CubeWidget(props) {
 
     }, [ timerReady, isSolving ]);
 
-
     const onSolve = useCallback((e) => {
 
         console.log("cube solved");
@@ -66,7 +69,6 @@ function CubeWidget(props) {
         }
 
     }, [ timerReady, isSolving ]);
-
 
     const onShuffle = useCallback(() => {
 
@@ -88,62 +90,69 @@ function CubeWidget(props) {
 
     }, [ timerReady, isSolving ]);
 
+    const onKBControllerBind = (e) => {
+        console.log(e.detail);
+        const event = new CustomEvent(cubeId + '-do-rotate', { detail: e.detail });
+        document.dispatchEvent(event);
+    }
+
     useEffect(() => {
         document.addEventListener(timerId + '-finish', onFinish);
         document.addEventListener(cubeId + '-touched', onTouch);
         document.addEventListener(cubeId + '-solved', onSolve);
         document.addEventListener(cubeId + '-shuffled', onShuffle );
         document.addEventListener(cubeId + '-reset', onReset );
+        document.addEventListener(kbControllerId + '-bind-pressed', onKBControllerBind);
 
         return (() => {
             document.removeEventListener(timerId + '-finish', onFinish);
             document.removeEventListener(cubeId + '-touched', onTouch);
             document.removeEventListener(cubeId + '-solved', onSolve);
-            document.removeEventListener(cubeId + '-shuffled', onShuffle );
-            document.removeEventListener(cubeId + '-reset', onReset );
+            document.removeEventListener(cubeId + '-shuffled', onShuffle);
+            document.removeEventListener(cubeId + '-reset', onReset);
+            document.removeEventListener(kbControllerId + '-bind-pressed', onKBControllerBind);
         });
     }, [ timerReady, isSolving ]);
 
-
     return (
         <>
-        <div className="rubiks-cube-widget">
-            <div className='rubiks-cube-widget-card rubiks-cube-widget-cube-card'>
-                <Cube id={ cubeId } size={ 3 } />
-            </div>
-
-            <div className='rubiks-cube-widget-timer rubiks-cube-widget-card'>
-                <Timer 
-                    id={ timerId }
-                />
-            </div>
-
-            <div className='rubiks-cube-widget-buttons rubiks-cube-widget-card'>
-                <div>
-                    <button className='widget-button shuffle-button' id={ shuffleId }
-                        onClick = { 
-                            () => {
-                                const event = new CustomEvent(cubeId + '-do-shuffle');
-                                document.dispatchEvent(event);
-                            }
-                        } >
-                        shuffle
-                    </button>
-
-                    <button className='widget-button reset-button' id={ resetId }
-                        onClick = {
-                            () => {
-                                const event = new CustomEvent(cubeId + '-do-reset');
-                                document.dispatchEvent(event);
-                            }
-                        } >
-                        reset
-                    </button>
+            <KeyboardController id={ kbControllerId } bindings={ bindings } />
+            <div className="rubiks-cube-widget">
+                <div className='rubiks-cube-widget-card rubiks-cube-widget-cube-card'>
+                    <Cube id={ cubeId } size={ 3 } />
                 </div>
-               
+
+                <div className='rubiks-cube-widget-timer rubiks-cube-widget-card'>
+                    <Timer 
+                        id={ timerId }
+                    />
+                </div>
+
+                <div className='rubiks-cube-widget-buttons rubiks-cube-widget-card'>
+                    <div>
+                        <button className='widget-button shuffle-button' id={ shuffleId }
+                            onClick = { 
+                                () => {
+                                    const event = new CustomEvent(cubeId + '-do-shuffle');
+                                    document.dispatchEvent(event);
+                                }
+                            } >
+                            shuffle
+                        </button>
+
+                        <button className='widget-button reset-button' id={ resetId }
+                            onClick = {
+                                () => {
+                                    const event = new CustomEvent(cubeId + '-do-reset');
+                                    document.dispatchEvent(event);
+                                }
+                            } >
+                            reset
+                        </button>
+                    </div>
+                
+                </div>
             </div>
-            
-        </div>
         </>
     );
 }
